36a9ada9856ce566c6447fd09dcfc3b7fd63e202
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-04-26 11:58:53 +0200

    snake module

--- main.lua
+++ main.lua
@@ -8,29 +8,14 @@ g_width = 0
 g_height = 0
 g_matrix = {}
 
-a_tick = 0
-b_tick = 0
-
--- affected by haste/slow items
-a_speed = 0.15 -- seconds
-b_speed = 0.15 -- seconds
-
-SNAKE_A = "SNAKE_A"
-SNAKE_B = "SNAKE_B"
+g_ended = false
 
 NORTH = "NORTH"
 SOUTH = "SOUTH"
 EAST  = "EAST"
 WEST  = "WEST"
 
-a_direction = EAST
-b_direction = EAST
-
-a_head_x = 0
-a_head_y = 0
-
-b_head_x = 0
-b_head_y = 0
+a_snake = require "snake"
 
 APPLE_SIZE = 4 -- number of snake segments eating one apple gives 
 
@@ -49,101 +34,17 @@ function love.load()
 		g_matrix[ y * g_width + x ] = {}
 	end
 	end
-	a_head_x = 10
-	a_head_y = 10
 
-	b_head_x = 10
-	b_head_y = 20
-
-	local head = M(a_head_x, a_head_y)
-	head.a =  true
-	head.a_prev_x = nil
-	head.a_prev_y = nil
-	head.a_next_x = a_head_x - 1
-	head.a_next_y = a_head_y
-	
-	local body = M(a_head_x-1, a_head_y)
-	body.a =  true
-	body.a_prev_x = a_head_x
-	body.a_prev_y = a_head_y
-	body.a_next_x = a_head_x - 2
-	body.a_next_y = a_head_y
-
-	local tail = M(a_head_x-2, a_head_y)
-	tail.a =  true
-	tail.a_prev_x = a_head_x - 1
-	tail.a_prev_y = a_head_y 
-	tail.a_next_x = nil
-	tail.a_next_y = nil
-
-	a_tail_x = a_head_x - 2
-	a_tail_y = a_head_y
-
-	M(b_head_x, b_head_y).b = true
+	a_snake.init("a")
 end
 
--------------------------------------------------------------------------------
-function update_snake_a()
-	local dx = 0
-	local dy = 0
-	if     a_direction == NORTH then
-		dx = 0
-		dy = -1
-	elseif a_direction == SOUTH then
-		dx = 0
-		dy = 1
-	elseif a_direction == EAST then
-		dx = 1
-		dy = 0
-	elseif a_direction == WEST then
-		dx = -1
-		dy = 0
-	end
-	local old_x = a_head_x
-	local old_y = a_head_y
-	local old_head = M(a_head_x, a_head_y)
-	a_head_x = math.floor((a_head_x + dx + g_width) % g_width)
-	a_head_y = math.floor((a_head_y + dy + g_height) % g_height)
-	local head = M(a_head_x, a_head_y)
-	if head.a then
-		-- snake dead
-		love.event.push("quit")
-	end 
-	head.a = true
-	head.a_prev_x = nil
-	head.a_prev_y = nil	
-	head.a_next_x = old_x
-	head.a_next_y = old_y
-	old_head.a_prev_x = a_head_x
-	old_head.a_prev_y = a_head_y
-	local tail = M(a_tail_x, a_tail_y)
-	if M(a_head_x, a_head_y).apples then
-		m_apples.a_eats(M(a_head_x, a_head_y).apples)
-		M(a_head_x, a_head_y).apples = nil
-	end
-	if not m_apples.a_digesting() then
-		tail.a = false
-		a_tail_x = tail.a_prev_x
-		a_tail_y = tail.a_prev_y
-	end
-end 
-
--------------------------------------------------------------------------------
-function update_snake_b()
-end 
 
 -------------------------------------------------------------------------------
 function love.update(dt)
-	a_tick = a_tick + dt
-	b_tick = b_tick + dt
-	if a_tick >= a_speed then
-		a_tick = a_tick - a_speed
-		update_snake_a()
+	if g_ended then
+		return 
 	end
-	if b_tick >= b_speed then
-		b_tick = b_tick - b_speed
-		update_snake_b()
-	end
+	a_snake.update(dt)
 	if not m_apples.full() then
 		local x = math.floor( love.math.random() * g_width )
 		local y = math.floor( love.math.random() * g_height )
@@ -159,21 +60,13 @@ function love.keypressed(key)
 	if key == "escape" then
 		love.event.push("quit")
 	elseif key == "up" then
-		if M(a_head_x, a_head_y).a_next_x ~= a_head_x then
-			a_direction = NORTH
-		end
+		a_snake.up()
 	elseif key == "down" then
-		if M(a_head_x, a_head_y).a_next_x ~= a_head_x then
-			a_direction = SOUTH
-		end
+		a_snake.down()
 	elseif key == "left" then
-		if M(a_head_x, a_head_y).a_next_y ~= a_head_y then
-			a_direction = WEST
-		end
+		a_snake.left()
 	elseif key == "right" then
-		if M(a_head_x, a_head_y).a_next_y ~= a_head_y then
-			a_direction = EAST
-		end
+		a_snake.right()
 	end
 end
 
@@ -183,12 +76,12 @@ function love.draw()
 		for y = 0, g_height-1 do 
 			if M(x, y).apples then
 				m_draw.apple(x, y)
-			elseif x == a_head_x and y == a_head_y then
+			elseif a_snake.is_head(x, y) then
 				m_draw.head_a(x, y)
-			elseif M(x, y).a then
+			elseif M(x, y).data then
 				m_draw.tail_a(x, y)	
-			elseif x == b_head_x and y == b_head_y then
-				m_draw.head_b(x, y)
+			--elseif x == b_head_x and y == b_head_y then
+			--	m_draw.head_b(x, y)
 			elseif x % 2 == 0 and y % 2 == 0 then
 				m_draw.background_2(x, y)
 			else
@@ -197,7 +90,7 @@ function love.draw()
 			m_draw.lines(x, y)
 		end
 	end
-	love.graphics.print("x=".. a_head_x ..", y=" .. a_head_y, 100, 100)
+	-- love.graphics.print("x=".. a_head_x ..", y=" .. a_head_y, 100, 100)
 end
 
 -------------------------------------------------------------------------------
---
+++ snake.lua
@@ -0,0 +1,146 @@
+--- @module snake
+local m_snake = {}
+
+local direction = EAST
+
+local tick = 0
+-- affected by haste/slow items
+local speed = 0.15 -- seconds
+
+local head_x = 0
+local head_y = 0
+
+local tail_x = 0
+local tail_y = 0
+
+local id = "x"
+
+local dead = false
+
+-------------------------------------------------------------------------------
+function m_snake.init(snake_id)
+	id = snake_id
+
+	head_x = 10
+	head_y = 10
+
+	local head = M(head_x, head_y)
+	head.data   =  snake_id
+	head.prev_x = nil
+	head.prev_y = nil
+	head.next_x = head_x - 1
+	head.next_y = head_y
+	
+	local body = M(head_x-1, head_y)
+	body.data   = snake_id
+	body.prev_x = head_x
+	body.prev_y = head_y
+	body.next_x = head_x - 2
+	body.next_y = head_y
+
+	local tail = M(head_x-2, head_y)
+	tail.data   = snake_id
+	tail.prev_x = head_x - 1
+	tail.prev_y = head_y 
+	tail.next_x = nil
+	tail.next_y = nil
+
+	tail_x = head_x - 2
+	tail_y = head_y
+end
+
+-------------------------------------------------------------------------------
+local function update_snake()
+	local dx = 0
+	local dy = 0
+	if     direction == NORTH then
+		dx = 0
+		dy = -1
+	elseif direction == SOUTH then
+		dx = 0
+		dy = 1
+	elseif direction == EAST then
+		dx = 1
+		dy = 0
+	elseif direction == WEST then
+		dx = -1
+		dy = 0
+	end
+	local old_x = head_x
+	local old_y = head_y
+	local old_head = M(head_x, head_y)
+	head_x = math.floor((head_x + dx + g_width) % g_width)
+	head_y = math.floor((head_y + dy + g_height) % g_height)
+	local head = M(head_x, head_y)
+	if head.data then
+		dead = true
+		g_ended = true
+	end 
+	head.data = id
+	head.prev_x = nil
+	head.prev_y = nil	
+	head.next_x = old_x
+	head.next_y = old_y
+	old_head.prev_x = head_x
+	old_head.prev_y = head_y
+	local tail = M(tail_x, tail_y)
+	if M(head_x, head_y).apples then
+		m_apples.a_eats(M(head_x, head_y).apples)
+		M(head_x, head_y).apples = nil
+	end
+	if not m_apples.a_digesting() then
+		tail.data = false
+		tail_x = tail.prev_x
+		tail_y = tail.prev_y
+	end
+end 
+
+-------------------------------------------------------------------------------
+function m_snake.update(dt)
+	tick = tick + dt
+	if tick >= speed then
+		tick = tick - speed
+		update_snake()
+	end
+end
+
+-------------------------------------------------------------------------------
+function m_snake.up()
+	if M(head_x, head_y).next_x ~= head_x then
+		direction = NORTH
+	end
+end
+
+-------------------------------------------------------------------------------
+function m_snake.down()
+	if M(head_x, head_y).a_next_x ~= head_x then
+		direction = SOUTH
+	end	
+end
+
+-------------------------------------------------------------------------------
+function m_snake.left()
+	if M(head_x, head_y).a_next_y ~= head_y then
+		direction = WEST
+	end
+end
+
+-------------------------------------------------------------------------------
+function m_snake.right()
+	if M(head_x, head_y).a_next_y ~= head_y then
+		direction = EAST
+	end	
+end
+
+-------------------------------------------------------------------------------
+function m_snake.is_head(x, y)
+	return x == head_x and y == head_y
+end
+
+-------------------------------------------------------------------------------
+function m_snake.is_dead()
+	return dead
+end
+
+-------------------------------------------------------------------------------
+return m_snake
